cd90582640ba9663925a6d1e960e0749a04d7150
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-05 23:52:22 -0700

    Added:Program For Finding Frequency Of Words In A Large Paragraph

--- src/hashtables/HashTable.java
+++ src/hashtables/HashTable.java
@@ -2,24 +2,29 @@ package hashtables;
 
 public class HashTable
 {
-	public static void main(String[] args) {
-        HashTableImpl<String, Integer> hashImpl = new HashTableImpl();
-        String message = "To be or not to be";
+    public static void main(String[] args) 
+    {
+	        HashTableImpl<String, Integer> hashImpl = new HashTableImpl();
+	        String message = "“Paranoids are not\r\n"
+	        		+ "paranoid because they are paranoid but\r\n"
+	        		+ "because they keep putting themselves\r\n"
+	        		+ "deliberately into paranoid avoidable\r\n"
+	        		+ "situations”";
 
-        //String to String[] conversion.
-        String[] messageArray = message.toLowerCase().split(" ");
+	        //String to String conversion.
+	        String[] messageArray = message.toLowerCase().split(" ");
 
-        //Iterate within the array
-        for(String word: messageArray) {
-            //Search for the word in the linked list
-            Integer value =  hashImpl.get(word);
-            if( value == null)
-                value = 1;
-            else
-                value = value + 1;
-            hashImpl.add(word , value);
-        }
-        //Display the linked list
-        System.out.println(hashImpl);
-    }
+	        //Iterate within the Array
+	        for(String word: messageArray) 
+	        {
+	            Integer value =  hashImpl.get(word);
+	            if( value == null)
+	                value = 1;
+	            else
+	                value = value + 1;
+	            hashImpl.add(word , value);
+	        }
+	        //Display  linked list
+	        System.out.println(hashImpl);
+	    }
 }
